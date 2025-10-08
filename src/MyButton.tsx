9e67f6fb9ca16d2100a66dbc414a582394443243
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-09 00:02:15 +1300

    feat(preact): update MyButton.tsx to use preact

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { type FunctionComponent } from 'preact'
 
 interface MyButtonProps {
   type?: 'primary'
 }
 
-export const MyButton: React.FC<MyButtonProps> = ({ type }) => {
-  return <button className="my-button">my button: type {type}</button>
+export const MyButton: FunctionComponent<MyButtonProps> = ({ type }) => {
+  return <button class="my-button">my button: type {type}</button>
 }
