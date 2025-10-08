58590dec76ca36951236f53134b53eb2c702b4ab
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-08 22:42:02 +1300

    fix: add eslint deps and config

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import eslintJs from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.js'],
+    extends: [
+      eslintJs.configs.recommended
+    ]
+  },
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    extends: [
+      tseslint.configs.recommended
+    ]
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: [
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime']
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+]);
