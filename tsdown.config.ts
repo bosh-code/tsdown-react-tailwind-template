c27b0929d6d8be3146e3b8a27369ab8b62cb7284
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-09 00:03:15 +1300

    feat(preact): update tsdown externals array to preact packages

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown';
 export default defineConfig([
   {
     entry: ['./src/index.ts'],
-    external: ['react', 'react/jsx-runtime'],
+    external: ['preact', 'preact/jsx-runtime', 'preact-compat'],
     platform: 'browser',
     dts: true,
     minify: process.env.NODE_ENV === 'prod',
