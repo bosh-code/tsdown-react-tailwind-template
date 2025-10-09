dc26deaa7b9321b35e06e485fd0eff7ce4cbcc8c
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-10 09:29:12 +1300

    feat(preact): add second button to playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,9 +1,11 @@
 import { MyButton } from '../../src'
+import { MyButton as BuiltButton } from '../../dist'
 
 export function App() {
   return (
     <>
       <MyButton type="primary" />
+      <BuiltButton />
     </>
   )
 }
