3ab2a8c0e9091f423ddadae45af33e8d44564bd6
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-12 16:09:27 +1300

    feat(styling): update playground styles

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,9 +1,12 @@
-import { MyButton } from '../../src'
+// import { MyButton } from '../../src'
+
+import { MyButton as BuiltButton } from '../../dist'
 
 export function App() {
   return (
     <>
-      <MyButton type="primary" />
+      {/*<MyButton type="primary" />*/}
+      <BuiltButton />
     </>
   )
 }
