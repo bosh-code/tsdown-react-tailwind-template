b81ff29ad65d7c7f37a6c5984c2361a935721ab2
Ryan Bosher <ryan.julian.bosher@icloud.com>
2025-10-12 16:20:41 +1300

    feat(tailwind): add tailwindcss to lib

--- src/MyButton.tsx
+++ src/MyButton.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-import './MyButton.css'
-
 interface MyButtonProps {
   type?: 'primary'
 }
 
 export const MyButton: React.FC<MyButtonProps> = ({ type }) => {
-  return <button className="my-button global-button">my button: type {type}</button>
+  return <button className="text-red-500">my button: type {type}</button>
 }
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'tsdown';
 import { injectCssPlugin } from '@bosh-code/tsdown-plugin-inject-css';
+import { tailwindPlugin } from '@bosh-code/tsdown-plugin-tailwindcss';
 
 export default defineConfig([
   {
@@ -10,7 +11,11 @@ export default defineConfig([
     minify: process.env.NODE_ENV === 'prod',
     sourcemap: true,
     plugins: [
-      injectCssPlugin()
+      injectCssPlugin(),
+      tailwindPlugin({
+          minify: process.env.NODE_ENV === 'prod'
+        }
+      )
     ]
   }
 ]);
